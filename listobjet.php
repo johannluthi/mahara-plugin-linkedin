407edaf2bbe288cc23fcb99407284e89f8658e04
johannluthi <johann.luthi@gmail.com>
2014-04-23 11:12:26 +0200

    clean the code

--- listobjet.php
+++ listobjet.php
@@ -1,26 +1,4 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$array = array(
-    array('name'=>'skills','attribute'=>'skill'),
-    array('name'=>'positions','attribute'=>'skill'),
-    array('name'=>'educations','attribute'=>'skill'),
-    array('name'=>'languages','attribute'=>'skill'),
-    array('name'=>'job-bookmarks','attribute'=>'skill'),
-    array('name'=>'suggestions','attribute'=>'skill'),
-    array('name'=>'publications','attribute'=>'skill'),
-    array('name'=>'recommendations-received','attribute'=>'skill'),
-    array('name'=>'patents','attribute'=>'skill'),
-    array('name'=>'certifications','attribute'=>'skill'),
-    array('name'=>'courses','attribute'=>'skill'),
-    array('name'=>'volunteer','attribute'=>'skill'),
-    array('name'=>'three-current-positions','attribute'=>'skill'),
-    array('name'=>'three-past-positions','attribute'=>'skill'),
-);
 $array = array('associations','skills','positions','educations','languages'
     ,'interests','job-bookmarks','suggestions','publications','recommendations-received'
     ,'patents','certifications','courses','volunteer','three-current-positions','three-past-positions');
